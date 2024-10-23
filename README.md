fb15501f13fcf55254c932a343450abb2c85757c
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-23 17:02:52 -0300

    update de arquivos crawl

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# projeto-scraping-mercado-livre
+# Projeto de ETL para Monitoramento de Preços e Produtos E-commerce
+
+## Descrição
+magine que uma marca de tênis deseja avaliar sua relevância e competitividade dentro do ecossistema de e-commerce, especificamente nas plataformas Mercado Livre ,Centauro e Netshoes . Para isso, é fundamental monitorar e analisar KPIs importantes do segmento de tênis, como preço médio, variações de preço e avaliação . A coleta e análise desses dados permitem à empresa tomar decisões estratégicas em relação ao pricing (estratégia de precificação) e relevância do produto no mercado.
+
+## Arquitetura
+Uma ETL em Python para Web Scraping 
+* Extração - Scrapy
+* Transformação e Load - Pandas
+* Banco de dados - Postgres
+* Dashboard - Streamlit
+
+## Diagrama
+![Diagrama](/pics/arquitetura.png)
---
+++ coleta/coleta/spiders/netshoes.py
@@ -0,0 +1,50 @@
+import scrapy
+from datetime import datetime
+
+class NetshoesSpider(scrapy.Spider):
+    name = "netshoes"
+    allowed_domains = ["www.netshoes.com.br"]
+    start_urls = ["https://www.netshoes.com.br/running/tenis-performance?genero=masculino"]
+    page_count = 1
+    max_pages = 10
+
+    def parse(self, response):
+        # Pegue os links para as páginas dos produtos
+        product_links = response.css('a.smarthint-tracking-card::attr(href)').getall()
+        for link in product_links:
+            yield response.follow(link, callback=self.parse_product)
+
+        # Verifica se deve continuar para a próxima página
+        '''
+        if self.page_count < self.max_page:
+            self.page_count += 1
+            next_page = f"{self.start_urls[0]}&departamento=running&tipo-de-produto=tenis-performance&nsCat=Artificial?page={self.page_count}"
+            if next_page:
+                yield scrapy.Request(url=next_page, callback=self.parse)
+        '''
+
+    def parse_product(self, response):
+        
+        product_attributes = response.css('ul.features--attributes li')
+        
+        if product_attributes:
+            brand_element = product_attributes[-1]
+            brand = brand_element.css('a::text').get()
+        else:
+            brand_name_list = response.css('h1.product-name::text').get().split()
+            if brand_name_list[1] == 'Masculino':
+                brand = brand_name_list[2]
+            else:
+                brand = brand_name_list[1]
+
+        yield {
+            'brand': brand,
+            'name': response.css('h1.product-name::text').get(),
+            'new_price_reais': response.css('div.price-box__saleInCents span.saleInCents-value::text').get().strip(),
+            'reviews_rating_number': response.css('div[aria-label="Avaliações"] div[aria-label="Média"]::text').get().strip(),
+            'reviews_amount': response.css('p[aria-label="Número de reviews"]::text').get().strip(),
+            'page_count': self.page_count,
+            '_source_name': self.name,
+            '_source_link': self.start_urls[0],
+            '_data_coleta': datetime.now()
+        }
