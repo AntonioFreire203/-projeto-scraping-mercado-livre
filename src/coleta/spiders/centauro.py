24b6997e9fc7c2277bfd4f324175e995931dd5fb
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-24 18:28:57 -0300

    Refactor Centauro spider: adjust typing and improve brand extraction logic

--- src/coleta/spiders/centauro.py
+++ src/coleta/spiders/centauro.py
@@ -1,30 +1,31 @@
 import scrapy
 from datetime import datetime
 import re
+from typing import List, Dict, Any,Optional
 
 class CentauroSpider(scrapy.Spider):
-    name = "centauro"
-    allowed_domains = ["www.centauro.com.br"]
-    start_urls = ["https://www.centauro.com.br/busca/tenis-corrida-masculino"]
-
-    page_count = 1
-    max_page = 10
+    name: str = "centauro"
+    allowed_domains: List[str] = ["www.centauro.com.br"]
+    start_urls: List[str] = ["https://www.centauro.com.br/busca/tenis-corrida-masculino"]
+    page_count: int = 1
+    max_page: int = 10
 
     def parse(self, response):
         
-        products = response.css('a[data-testid="grid-product-card"]')
+        products: List[scrapy.Selector] = response.css('a[data-testid="grid-product-card"]')
         
         for product in products:
             # Extraindo a avaliação do texto
-            rating_div = product.css('div[data-testid="product-rating"]::attr(aria-label)').get()
-            rating = None
+            rating_div: Optional[str] = product.css('div[data-testid="product-rating"]::attr(aria-label)').get()
+            rating: Optional[str] = None
+
             if rating_div:         
                 match = re.search(r'avaliado em (\d+(\.\d+)?) de 5 estrelas', rating_div)
                 if match:
-                    rating = match.group(1)
+                    rating:str = match.group(1)
 
             # Extraindo o brand e name do texto
-            brand_and_name = product.css('p.Typographystyled__Paragraph-sc-bdxvrr-1::text').get()
+            brand_and_name:Optional[str] = product.css('p.Typographystyled__Paragraph-sc-bdxvrr-1::text').get()
             if brand_and_name:
                 brand, name = self.extract_brand_and_name(brand_and_name)
             else:
@@ -48,7 +49,7 @@ class CentauroSpider(scrapy.Spider):
             if next_page:
                 yield scrapy.Request(url=next_page, callback=self.parse)
 
-    def extract_brand_and_name(self, text):
+    def extract_brand_and_name(self, text:str) :
         parts = text.split(" ", 1)
         if len(parts) >= 2:
             brand = parts[1].split(" ")[0].capitalize()
